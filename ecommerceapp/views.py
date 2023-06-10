4ab061816fb9125bca005a8af30510b73c0932e4
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-06-10 06:30:06 +0300

    diplay bootstrap and templates parts

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def index(request):
+    # return render(request, "ecommerceapp/index.html", {})
+    return render(request, "index.html", {})
+
+
+def contact(request):
+    # return render(request, "ecommerceapp/index.html", {})
+    return render(request, "ecommerceapp/contact.html", {})
+
+def about(request):
+    # return render(request, "ecommerceapp/index.html", {})
+    return render(request, "ecommerceapp/about.html", {})
