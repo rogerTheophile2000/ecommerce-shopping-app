679bff0d6931f8ad5644dc0910e31012fd16af6d
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-06-12 08:37:18 +0300

    adding create user part

--- connexion/views.py
+++ connexion/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
+
+
+
 
 # Create your views here.
 def signup(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password1']
+        confirm_password = request.POST['password2']
+        if password != confirm_password:
+            # HttpResponse("pass incorect")
+            # messages.warning(request, "Password is not Matching")
+            return render(request, "connexion/signup.html", {})
+        try:
+            if User.objects.get(username = email):
+                # HttpResponse("email exist")
+                # messages.warning(request, "Email is taken")
+                return render(request, "connexion/signup.html", {})
+        except Exception as identifier:
+            pass
+
+        user = User.objects.create_user(email, email, password)
+        # user.is_active = False
+        user.save()
     return render(request, "connexion/signup.html", {})
+
 
 def handlelogin(request):
     return render(request, "connexion/login.html", {})
