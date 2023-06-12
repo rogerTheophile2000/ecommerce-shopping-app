23c3f72cf5825a9640f3c9f86d7b8fe4fe7cde30
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-06-12 20:03:01 +0300

    adding redirecting on signup

--- connexion/views.py
+++ connexion/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
-
+from django.contrib import messages
 
 
 
@@ -12,12 +12,12 @@ def signup(request):
         confirm_password = request.POST['password2']
         if password != confirm_password:
             # HttpResponse("pass incorect")
-            # messages.warning(request, "Password is not Matching")
+            messages.warning(request, "Password is not Matching")
             return render(request, "connexion/signup.html", {})
         try:
             if User.objects.get(username = email):
                 # HttpResponse("email exist")
-                # messages.warning(request, "Email is taken")
+                messages.warning(request, "Email is taken")
                 return render(request, "connexion/signup.html", {})
         except Exception as identifier:
             pass
